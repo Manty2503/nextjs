0ebfdff562477fd7345d6f8c33136ee285f40da1
Manty2503 <119813195+Manty2503@users.noreply.github.com>
2025-02-13 22:09:54 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,6 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
-import { useTheme } from "next-themes";
 import { ModeToggle } from "@/components/mode-toggle";
 import { Plus, Edit, Trash, Calendar, Flag, CheckCircle } from "lucide-react"; // Import icons
 import { motion } from "framer-motion"; // For animations
